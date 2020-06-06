45d222eb27dd3e6850fb37301fefa48a0ea03fb4
Haziel-Kau <65087494+Haziel-Kau@users.noreply.github.com>
2020-06-06 14:53:22 -0600

    @Configuration-@Bean Haziel-Kau

--- SpringConAnnotations/src/es/pildoras/pruebaannotation/UsoAnnotations2.java
+++ SpringConAnnotations/src/es/pildoras/pruebaannotation/UsoAnnotations2.java
@@ -3,6 +3,7 @@ package es.pildoras.pruebaannotation;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class UsoAnnotations2 {
 
 	public static void main(String[] args) {
