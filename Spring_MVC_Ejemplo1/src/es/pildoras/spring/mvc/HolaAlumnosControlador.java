5c04d66520b091361e2a5f042a9377b42c9d7d25
ARI <ARI@192.168.1.13>
2020-06-06 22:34:06 -0500

    Uso del @RequestParam

--- Spring_MVC_Ejemplo1/src/es/pildoras/spring/mvc/HolaAlumnosControlador.java
+++ Spring_MVC_Ejemplo1/src/es/pildoras/spring/mvc/HolaAlumnosControlador.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HolaAlumnosControlador {
@@ -23,20 +24,15 @@ public class HolaAlumnosControlador {
 
 	}
 
+	//public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
 	@RequestMapping("/procesarFormulario2")
-	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
-
-		String nombre = request.getParameter("nombreAlumno");
-
+	public String otroProcesoFormulario(@RequestParam("nombreAlumno") String nombre,Model modelo) {
+		//String nombre = request.getParameter("nombreAlumno")
 		nombre += " es el mejor alumno";
-
 		String mensajeFinal = "¿Quién es el mejor alumno? " + nombre;
-
 		// Agregando Informacio al modelo
 		modelo.addAttribute("mensajeClaro", mensajeFinal);
-
 		return "HolaAlumnosSpring";
-
 	}
 
 }
