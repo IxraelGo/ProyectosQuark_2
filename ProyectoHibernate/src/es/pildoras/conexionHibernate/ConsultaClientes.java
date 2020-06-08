1c578cf721cc453d4e4dfd40741a13821256933c
ISRAEL <ISRAEL@192.168.1.20>
2020-06-07 20:40:07 -0500

    Consulta de tipo SELECT con criterios

---
+++ ProyectoHibernate/src/es/pildoras/conexionHibernate/ConsultaClientes.java
@@ -0,0 +1,61 @@
+package es.pildoras.conexionHibernate;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ConsultaClientes {
+	
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		
+		
+		SessionFactory miFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Clientes.class).buildSessionFactory();
+		
+		Session miSession= miFactory.openSession();
+		
+		try {
+			
+			//comenzar sesión
+			miSession.beginTransaction();
+			
+			//Consulta de clientes
+			List<Clientes> losClientes=miSession.createQuery("from Clientes").getResultList();
+			
+			//mostrar los clientes
+			recorreClientesConsultados(losClientes);
+			
+			//consulta: dame los Gómez
+			losClientes=miSession.createQuery("from Clientes cl where cl.apellidos='Gómez'").getResultList();
+			
+			recorreClientesConsultados(losClientes);
+			
+			//muestra los que viven en Gran Via o que se apellidan Delgado
+			losClientes=miSession.createQuery("from Clientes cl where cl.apellidos='Delgado'" + 
+			"or cl.direccion='Gran Vía'").getResultList();
+			recorreClientesConsultados(losClientes);
+			
+			//commit
+			miSession.getTransaction().commit();
+			//cerrar la sesión
+			miSession.close();
+			
+		}finally {
+			miFactory.close();
+		}
+		
+	}
+
+	private static void recorreClientesConsultados(List<Clientes> losClientes) {
+		for (Clientes unCliente:losClientes) {
+			
+			System.out.println(unCliente);
+			
+		}
+	}
+
+	
+}
